ca08f1a6965679b809babc3f961793c7caac86e9
Zakhar Rud <zahar8849@gmail.com>
2023-04-29 17:53:08 +0300

    React link

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,6 +8,7 @@ const Profile = (props) => {
     <div className={style.profile}>
       <ProfileInfo/>
       <MyPosts posts={props.posts}/>
+      <a href="https://react.dev/"></a>
     </div>
   )
 }
